a44c512e111ccfadcd6e5f5ba5c760c36f713580
Hsing <skylinegtr0224@gmail.com>
2023-10-11 00:45:28 +0800

    新增顏色設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const defaultColors = require('tailwindcss/colors');
+const colors = {
+  ...defaultColors,
+  ...{
+    'body-bg': '#182a4f',
+    'name': '#fadb80',
+  },
+}
+
 export default {
-  content: [
-    "./index.html"
-  ],
+  content: ['index.html', './src/*.{js,html}'],
   theme: {
+    fontFamily: {
+      'body': ['"Noto Sans TC"', 'sans-serif'],
+      'roboto': ['"Roboto"', 'sans-serif'],
+    },
+    colors: colors,
     extend: {},
   },
   plugins: [],
