a0f8a5a4c4d296893bb5e77ab0d4e8b3d38cee2e
Hsing <skylinegtr0224@gmail.com>
2023-10-15 00:59:26 +0800

    新增圖片

--- src/main.js
+++ src/main.js
@@ -1,47 +1,59 @@
-import audioSrc from './pokemon.mp3';
-const maskImg = document.getElementById('maskImg');
-const showImg = document.getElementById('showImg');
-const title = document.getElementById('title');
-const question = document.getElementById('question');
-const answer = document.getElementById('answer');
-const whoami = document.getElementById('whoami');
-const button = document.getElementById('button');
+import questionAudioSrc from './pokemon.mp3';
+import answerSrc from './answer.mp3';
 
+function loadHandler() {
+  const welcome = document.getElementById('welcome');
+  const welcomeButton = document.getElementById('welcomeButton');
+  // const maskImg = document.getElementById('maskImg');
+  // const showImg = document.getElementById('showImg');
+  const img = document.getElementById('img');
+  const title = document.getElementById('title');
+  const question = document.getElementById('question');
+  const answer = document.getElementById('answer');
+  const whoami = document.getElementById('whoami');
+  const button = document.getElementById('button');
+  const questionAudio = document.createElement('audio');
+  questionAudio.id = 'welcomeBackgroundMusic';
+  questionAudio.src = questionAudioSrc;
+  document.body.appendChild(questionAudio);
+  const answerAudio = document.createElement('audio');
+  answerAudio.id = 'answerBackgroundMusic';
+  answerAudio.src = answerSrc;
+  document.body.appendChild(answerAudio);
+  const welcomeBackgroundMusic = document.getElementById('welcomeBackgroundMusic');
+  const answerBackgroundMusic = document.getElementById('answerBackgroundMusic');
 
-const audio = document.createElement('audio');
-audio.id = 'backgroundMusic';
-audio.src = audioSrc;
-audio.autoplay = true;
-document.body.appendChild(audio);
+  function questionShow() {
+    setTimeout(() => {
+      title.classList.add('before:scale-x-0');
+    }, 3000);
+    setTimeout(() => {
+      whoami.classList.remove('opacity-0');
+      button.classList.remove('opacity-0');
+    }, 3500);
+  }
 
-const backgroundMusic = document.getElementById('backgroundMusic');
+  function showWho() {
+    img.classList.remove('brightness-0');
 
-function loadShow() {
-  setTimeout(() => {
-    title.classList.add('before:scale-x-0');
-  }, 3000);
-  setTimeout(() => {
-    whoami.classList.remove('opacity-0');
-    button.classList.remove('opacity-0');
-  }, 3500);
+    whoami.classList.add('opacity-0');
+    setTimeout(() => {
+      question.classList.add('opacity-0');
+      answer.classList.remove('opacity-0');
+    }, 3000)
+  }
+
+  welcomeButton.addEventListener('click', () => {
+    welcome.classList.add('opacity-0', 'pointer-events-none');
+    welcomeBackgroundMusic.play();
+    questionShow();
+  });
+
+  button.addEventListener('click', () => {
+    showWho();
+    button.remove();
+    answerBackgroundMusic.play();
+  });
 }
 
-function showWho() {
-  maskImg.classList.add('opacity-0');
-  showImg.classList.remove('opacity-0');
-
-  question.classList.add('opacity-0');
-  whoami.classList.add('opacity-0');
-  answer.classList.remove('opacity-0');
-}
-
-window.addEventListener('load', () => {
-  loadShow();
-});
-
-button.addEventListener('click', () => {
-  showWho();
-  backgroundMusic.play();
-
-  button.remove();
-});
+window.addEventListener('load', loadHandler);
