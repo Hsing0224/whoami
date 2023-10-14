221baa2694ea70c881188da8fb336e9eebca769f
Hsing <skylinegtr0224@gmail.com>
2023-10-14 19:26:55 +0800

    新增RWD

--- src/main.js
+++ src/main.js
@@ -1,21 +1,28 @@
+const backgroundMusic = document.getElementById('backgroundMusic');
 const maskImg = document.getElementById('maskImg');
 const showImg = document.getElementById('showImg');
+const title = document.getElementById('title');
 const question = document.getElementById('question');
-const whoami = document.getElementById('whoami');
 const answer = document.getElementById('answer');
+const whoami = document.getElementById('whoami');
 const button = document.getElementById('button');
 
 function loadShow() {
-  question.classList.add('before:scale-x-0');
+  // backgroundMusic.play();
+  setTimeout(() => {
+    title.classList.add('before:scale-x-0');
+  }, 3000);
   setTimeout(() => {
     whoami.classList.remove('opacity-0');
-  }, 500);
+    button.classList.remove('opacity-0');
+  }, 3500);
 }
 
 function showWho() {
   maskImg.classList.add('opacity-0');
   showImg.classList.remove('opacity-0');
 
+  question.classList.add('opacity-0');
   whoami.classList.add('opacity-0');
   answer.classList.remove('opacity-0');
 }
